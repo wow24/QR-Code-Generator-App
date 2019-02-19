9855cd84fadeb82f8caa305338df8adefcde34f9
wow24 <wow24art@outlook.com>
2019-02-20 03:03:52 +0530

    QR Code Generetor App

---
+++ QR Code Generator App/Form1.Designer.vb
@@ -0,0 +1,90 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class QRCG
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pic = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.btnGenerate = New System.Windows.Forms.Button()
+        CType(Me.pic, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'pic
+        '
+        Me.pic.BackColor = System.Drawing.Color.White
+        Me.pic.Location = New System.Drawing.Point(12, 12)
+        Me.pic.Name = "pic"
+        Me.pic.Size = New System.Drawing.Size(537, 358)
+        Me.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage
+        Me.pic.TabIndex = 0
+        Me.pic.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 415)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Input Text :"
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(96, 393)
+        Me.txtInput.Multiline = True
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(453, 64)
+        Me.txtInput.TabIndex = 2
+        '
+        'btnGenerate
+        '
+        Me.btnGenerate.Location = New System.Drawing.Point(12, 478)
+        Me.btnGenerate.Name = "btnGenerate"
+        Me.btnGenerate.Size = New System.Drawing.Size(537, 47)
+        Me.btnGenerate.TabIndex = 3
+        Me.btnGenerate.Text = "Generate"
+        Me.btnGenerate.UseVisualStyleBackColor = True
+        '
+        'QRCG
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(566, 537)
+        Me.Controls.Add(Me.btnGenerate)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.pic)
+        Me.Name = "QRCG"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "QR Code Generator"
+        CType(Me.pic, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents pic As PictureBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtInput As TextBox
+    Friend WithEvents btnGenerate As Button
+End Class
---
+++ QR Code Generator App/Form1.vb
@@ -0,0 +1,10 @@
+﻿Imports QRCoder
+
+Public Class QRCG
+    Private Sub btnGenerate_Click(sender As Object, e As EventArgs) Handles btnGenerate.Click
+        Dim gen As New QRCodeGenerator
+        Dim data = gen.CreateQrCode(txtInput.Text, QRCodeGenerator.ECCLevel.Q)
+        Dim Code As New QRCode(data)
+        pic.Image = Code.GetGraphic(6)
+    End Sub
+End Class
